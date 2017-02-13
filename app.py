725e10733d4e06cf3b31e15580bb283806793f33
jimmy <tangzhenpeng@gmail.com>
2017-02-13 20:14:55 +0800

    second commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import tornado.ioloop
+import tornado.web
+import yaml
+from module.db_control import db
+
+f=open("config.yaml")
+config=yaml.load(f)
+db=db(config["sms_sqlite3"])
+
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        if self.get_argument("get_token")=="get_token_md5":
+            sms_list = db.read_sms()
+            sms_list.reverse()
+            self.render("templates/index.html",smslist=sms_list)
+
+    def post(self):
+        #print self.request.body
+        if self.get_argument("post_token")=="post_token_md5":
+            phone=self.get_argument("number")#.encode("utf-8")
+            datetime=self.get_argument("datetime")#.encode("utf-8")
+            body=self.get_argument("text")#.encode("utf-8")
+            print body
+            db.write_sms(phone,datetime,body)
+
+
+class SmsHandler(tornado.web.RequestHandler):
+    def get(self):
+        sms_list=getSMSList()
+        sms_test_list=get_the_test_sms(sms_list)
+        self.render("templates/index.html",smslist=sms_test_list)
+
+application = tornado.web.Application([
+    (r"/test", MainHandler),
+	(r"/showyoursms",SmsHandler),
+    (r"/(apple-touch-icon-120x120\.png)", tornado.web.StaticFileHandler, {"path": "static/apple-touch-icon"}),
+    (r"/(apple-touch-icon-120x120-precomposed\.png)", tornado.web.StaticFileHandler, {"path": "static/apple-touch-icon"}),
+    (r"/css/(.*)", tornado.web.StaticFileHandler, {"path": "static/css"}),
+    (r"/js/(.*)", tornado.web.StaticFileHandler, {"path": "static/js"}),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
---
+++ module/db_control.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+#db_file="sms.db"
+#db_file="sms.db"
+class db():
+    def __init__(self,db_file):
+        self.db_file=db_file
+
+    def write_sms(self,phone,datetime,body):
+        data=(phone,datetime,body)
+        con = sqlite3.connect(self.db_file)
+        cu = con.cursor()
+        #cu.execute("create table inbox(phone text,datetime text,body text)")
+        cu.execute("insert into inbox values (?,?,?)",data)
+        con.commit()
+        con.close()
+
+    def read_sms(self):
+        con=sqlite3.connect(self.db_file)
+        cu=con.cursor()
+        cu.execute("select * from inbox")
+        t=cu.fetchall()
+        con.close()
+        return t
+
+    def del_all(self):
+        con=sqlite3.connect(self.db_file)
+        cu=con.cursor()
+        cu.execute("delete from inbox")
+        con.commit()
+        con.close()
+
+
+#t=db(db_file)
+#del_all()
+#write_sms("10010","2017-2-11 14:09","hello world")
+#print t.read_sms()
+
+
+
+
