831433acb11c656d05d04966d58d8f21c30b3403
jimmy <tangzhenpeng@gmail.com>
2017-02-15 21:01:49 +0800

    update show sms by one number one sms

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class MainHandler(tornado.web.RequestHandler):
         if self.get_argument("get_token")==config["get_token"]:
             sms_list = db.get_all_numbers_one_sms(db.read_sms())
             sms_list.reverse()
-            self.render("templates/test.html",smslist=sms_list)
+            self.render("templates/test.html",smslist=sms_list,get_token=config["get_token"])
 
     def post(self):
         #print self.request.body
@@ -32,9 +32,24 @@ class SmsHandler(tornado.web.RequestHandler):
         sms_test_list.reverse()
         self.render("templates/test.html",smslist=sms_test_list)
 
+class NumberHandler(tornado.web.RequestHandler):
+    def get(self):
+        if self.get_argument("get_token")==config["get_token"] and self.get_argument("number")!="":
+            number=self.get_argument("number")
+            sms_list = db.read_sms()
+            sms_list.reverse()
+            number_list=[]
+            for k in sms_list:
+                if k[0]==number:
+                    number_list.append(k)
+            self.render("templates/number.html",
+                        smslist=number_list,
+                        title=number)
+
 application = tornado.web.Application([
     (r"/test", MainHandler),
 	(r"/showyoursms",SmsHandler),
+    (r"/number",NumberHandler),
     (r"/(apple-touch-icon-120x120\.png)", tornado.web.StaticFileHandler, {"path": "static/apple-touch-icon"}),
     (r"/(apple-touch-icon-120x120-precomposed\.png)", tornado.web.StaticFileHandler, {"path": "static/apple-touch-icon"}),
     (r"/css/(.*)", tornado.web.StaticFileHandler, {"path": "static/css"}),
